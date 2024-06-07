e49566b526375ee37b3417c9957348effec2d987
1riz <146616594+1riz@users.noreply.github.com>
2024-06-07 17:17:20 +0200

    Update Rofi config

--- rofi/scripts/files.sh
+++ rofi/scripts/files.sh
@@ -5,7 +5,7 @@ echo -en "\0prompt\x1ffiles\n"
 if [[ ${ROFI_RETV} == 0 ]]; then
   fd -u -c never -E '.cache' -E '.git'
 elif [[ ${ROFI_RETV} == 1 ]]; then
-  coproc { alacritty --class "alacritty-floating" -T "xdg-open" -e xdg-open "${1}"; }
+  coproc { alacritty --config-file ~/.config/alacritty/alacritty_floating.toml -T "xdg-open" -e xdg-open "${1}"; }
 fi
 
 exit 0
