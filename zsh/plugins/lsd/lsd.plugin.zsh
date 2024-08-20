0df4ae9b7905f25612132167260506caee261774
1riz <146616594+1riz@users.noreply.github.com>
2024-08-20 21:08:03 +0200

    Update ZSH config

--- zsh/plugins/lsd/lsd.plugin.zsh
+++ zsh/plugins/lsd/lsd.plugin.zsh
@@ -6,5 +6,7 @@ export LS_COLORS="${LS_COLORS}*.z=00;00:"
 #
 # Aliases
 #
-alias la='lsd -l'
-alias lla='lsd -l --git --total-size'
+# alias la='lsd -l'
+alias la='la -l'
+alias lla='lsd -l'
+# alias lla='lsd -l --git --total-size'
