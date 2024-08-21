32e1a2241331b2e4c71b31f93e16d2c9e60f6899
1riz <146616594+1riz@users.noreply.github.com>
2024-08-21 21:58:35 +0200

    Update Labwc config

--- rofi/scripts/apps.sh
+++ rofi/scripts/apps.sh
@@ -4,16 +4,18 @@ echo -en "\0prompt\x1fapps\n"
 
 if [[ ${ROFI_RETV} == 0 ]]; then
 
-  echo -en "alacritty\0display\x1f󰆍  Alacritty\n"
-  echo -en "firefox\0display\x1f  Firefox\n"
-  echo -en "waterfox\0display\x1f󰖟  Waterfox\n"
-  echo -en "tor\0display\x1f  TorBrowser\n"
-  echo -en "nvim\0display\x1f  Neovim\n"
-  echo -en "btop\0display\x1f  BTop\n"
-  echo -en "ncspot\0display\x1f  NCSpot\n"
-  echo -en "nnn\0display\x1f  NNN\n"
-  echo -en "qalc\0display\x1f  Qalc\n"
-  echo -en "fzf\0display\x1f  FZF\n"
+  echo -en "alacritty\0display\x1f Alacritty\x1ficon\x1falacritty\n"
+  echo -en "firefox\0display\x1f Firefox\x1ficon\x1ffirefox\n"
+  echo -en "waterfox\0display\x1f Waterfox\x1ficon\x1fwaterfox\n"
+  echo -en "nvim\0display\x1f Neovim\x1ficon\x1fneovim\n"
+  echo -en "thunar\0display\x1f Thunar\x1ficon\x1fthunar\n"
+  echo -en "btop\0display\x1f BTop\x1ficon\x1fbtop\n"
+  echo -en "htop\0display\x1f HTop\x1ficon\x1fhtop\n"
+  echo -en "ncspot\0display\x1f NCSpot\x1ficon\x1fncspot\n"
+  echo -en "nnn\0display\x1f NNN\x1ficon\x1fnnn\n"
+  echo -en "qalc\0display\x1f Qalc\x1ficon\x1faccessories-calculator\n"
+  echo -en "qemu\0display\x1f QEMU\x1ficon\x1fqemu\n"
+  echo -en "dosbox\0display\x1f Dosbox\x1ficon\x1fdosbox\n"
 
 elif [[ ${ROFI_RETV} == 1 ]]; then
 
@@ -27,26 +29,29 @@ elif [[ ${ROFI_RETV} == 1 ]]; then
     waterfox)
       coproc { /opt/waterfox/waterfox; }
       ;;
-    tor)
-      coproc { /opt/tor-browser/Browser/start-tor-browser --detach; }
-      ;;
     nvim)
-      coproc { alacritty --config-file ~/.config/alacritty/alacritty_blue.toml -T "nvim" -e nvim; }
+      coproc { alacritty --config-file ~/.config/alacritty/alacritty_nightfox.toml -o 'window.class.general="neovim"' -o 'window.dimensions.lines=50' -o 'window.dimensions.columns=140' -e nvim; }
+      ;;
+    thunar)
+      coproc { thunar; }
       ;;
     btop)
-      coproc { alacritty -T "btop" -e btop; }
+      coproc { alacritty -T "btop" -o 'window.class.general="btop"' -e btop; }
       ;;
     ncspot)
-      coproc { alacritty --config-file ~/.config/alacritty/alacritty_floating.toml -T "ncspot" -e ncspot; }
+      coproc { alacritty -T "ncspot" -o 'window.class.general="ncspot"' -e ncspot; }
       ;;
     nnn)
-      coproc { alacritty --config-file ~/.config/alacritty/alacritty_floating_blue.toml -T "nnn" -e nnn -deoQTv; }
+      coproc { alacritty --config-file ~/.config/alacritty/alacritty_nightfox.toml -T "nnn" -o 'window.class.general="nnn"' -e nnn -deoQTv; }
       ;;
     qalc)
-      coproc { alacritty --config-file ~/.config/alacritty/alacritty_floating_blue.toml -o 'window.dimensions.lines=20' -o 'window.dimensions.columns=100' -T "qalc" -e qalc -c; }
+      coproc { alacritty --config-file ~/.config/alacritty/alacritty_nightfox.toml -T "qalc" -o 'window.class.general="qcalc"' -o 'window.dimensions.lines=20' -o 'window.dimensions.columns=100' -e qalc -c; }
       ;;
-    fzf)
-      coproc { alacritty --config-file ~/.config/alacritty/alacritty_floating.toml -T "fzf" -e fzf --bind 'enter:execute(bat --paging=always {})'; }
+    qemu)
+      coproc { ~/Projects/QEMU/openbsd/bin/run.sh; }
+      ;;
+    dosbox)
+      coproc { dosbox ~/Projects/DOS/devel; }
       ;;
   esac
 
--- rofi/scripts/files.sh
+++ rofi/scripts/files.sh
@@ -3,9 +3,10 @@
 echo -en "\0prompt\x1ffiles\n"
 
 if [[ ${ROFI_RETV} == 0 ]]; then
-  fd -u -c never -E '.cache' -E '.git'
+  # fd -u -c never -E '.cache' -E '.git'
+  fd -c never
 elif [[ ${ROFI_RETV} == 1 ]]; then
-  coproc { alacritty --config-file ~/.config/alacritty/alacritty_floating.toml -T "xdg-open" -e xdg-open "${1}"; }
+  coproc { alacritty -T "xdg-open" -e xdg-open "${1}"; }
 fi
 
 exit 0
