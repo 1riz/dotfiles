00a9e923241e13d883bea5ee704172c17f46b5bf
1riz <146616594+1riz@users.noreply.github.com>
2025-09-25 18:16:59 +0200

    Update XDG-MIME apps config

--- mime/default-apps.sh
+++ mime/default-apps.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
 rm -f ~/.config/mimeapps.list
-
 xdg-settings set default-web-browser firefox.desktop
-
-xdg-mime default org.pwmt.zathura-pdf-poppler.desktop application/pdf
-
-xdg-mime default imv.desktop image/bmp image/gif image/jpeg image/jpg image/pjpeg image/png image/tiff image/x-bmp image/x-pcx image/x-png image/x-portable-anymap image/x-portable-bitmap image/x-portable-graymap image/x-portable-pixmap image/x-tga image/x-xbitmap image/heif image/avif
-
 cat ~/.config/mimeapps.list
